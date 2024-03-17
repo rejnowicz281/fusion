e09267340bca9696f325595b8eace934b1705721
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-03-18 00:13:03 +0100

    Remove BackButton from settings

--- components/general/menubar/settings/index.tsx
+++ components/general/menubar/settings/index.tsx
@@ -1,6 +1,5 @@
 import signOut from "@/actions/auth/modify/sign-out";
 import SubmitButton from "../../submit-button";
-import BackButton from "./back-button";
 import DeleteAccountButton from "./delete-account-button";
 import ThemeButton from "./theme-button";
 import TogglePresenceButton from "./toggle-presence-button";
@@ -8,7 +7,6 @@ import TogglePresenceButton from "./toggle-presence-button";
 const Settings = () => {
     return (
         <div className="flex-1 flex flex-col">
-            <BackButton />
             <ThemeButton />
             <TogglePresenceButton />
             <DeleteAccountButton demoUserId={process.env.DEMO_USER_ID!} />
