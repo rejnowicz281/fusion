be905d73acd1de5bfe8b33d0feddcdf570c772b8
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-03-20 23:49:39 +0100

    Increase queryClient staleTime property

--- providers/query-provider.tsx
+++ providers/query-provider.tsx
@@ -10,9 +10,7 @@ function makeQueryClient() {
     return new QueryClient({
         defaultOptions: {
             queries: {
-                // With SSR, we usually want to set some default staleTime
-                // above 0 to avoid refetching immediately on the client
-                staleTime: 60 * 1000,
+                staleTime: Infinity, // Data is always considered fresh
             },
         },
     });
