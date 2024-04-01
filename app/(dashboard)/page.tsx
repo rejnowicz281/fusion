8d67d3496594d7466f6cb6d60ed9b3a363caa12a
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-04-01 12:35:53 +0200

    Update home page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,10 @@
-const Home = () => <div>Home</div>;
+import FusionSvg from "@/components/general/fusion-svg";
+
+const Home = () => (
+    <div className="flex gap-9 items-center flex-1 justify-center">
+        <FusionSvg size={150} />
+        <h1 className="text-9xl font-semibold tracking-wide">Fusion</h1>
+    </div>
+);
 
 export default Home;
