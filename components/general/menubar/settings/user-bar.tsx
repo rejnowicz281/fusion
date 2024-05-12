d425211d2b24edcd9006f0fe2ad79db8032a1f18
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2024-05-12 19:07:28 +0200

    Add a special message for demo users

--- components/general/menubar/settings/user-bar.tsx
+++ components/general/menubar/settings/user-bar.tsx
@@ -10,11 +10,17 @@ const UserBar = () => {
                 <Avatar aiMode={user.ai_mode} avatarSize={80} markerSize={18} userId={user.id} src={user.avatar_url} />
             </div>
 
-            <div className="text-center">
+            <div className="text-center px-2 sm:px-0">
                 <div className="truncate dark:font-semibold">{user.display_name}</div>
-                {user.email !== user.display_name && (
+                {user.email === "demo@demo.demo" ? (
+                    <div className="text-sm font-semibold text-gray-500">
+                        You are using a demo account. Account updating, AI Mode and Custom AI Prompts are disabled.
+                        <br />
+                        To enable these features, please log in using an email.
+                    </div>
+                ) : user.email !== user.display_name ? (
                     <div className="truncate text-sm font-semibold text-gray-500">{user.email}</div>
-                )}
+                ) : null}
             </div>
         </div>
     );
