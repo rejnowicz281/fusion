cb5860602d6a5282833ee345f707ceeb45eb8ee2
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2026-04-02 00:37:32 +0200

    fix realtime

--- hooks/use-realtime.ts
+++ hooks/use-realtime.ts
@@ -1,56 +1,72 @@
 import useAuthContext from "@/providers/auth-provider";
 import { createClient } from "@/utils/supabase/client";
+import { RealtimeChannel } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const useRealtime = () => {
     const { user: currentUser } = useAuthContext();
-
     const router = useRouter();
     const supabase = createClient();
 
+    const usersChannelRef = useRef<RealtimeChannel | null>(null);
+    const messagesChannelRef = useRef<RealtimeChannel | null>(null);
+
     useEffect(() => {
-        const usersChannel = supabase
-            .channel("users")
-            .on(
-                "postgres_changes",
-                {
-                    schema: "public",
-                    event: "*",
-                    table: "users",
-                    filter: `id=neq.${currentUser.id}` // only listen for changes not related to the current user (those are revalidated on the server-side)
-                },
-                (payload) => {
-                    console.log("User Change received, refreshing router", payload);
-                    router.refresh();
-                }
-            )
-            .subscribe();
+        if (!currentUser) return;
 
-        const messagesChannel = supabase
-            .channel("messages")
-            .on(
-                "postgres_changes",
-                {
-                    schema: "public",
-                    event: "*",
-                    table: "messages"
-                },
-                (payload) => {
-                    console.log("Messages Change received, refreshing router", payload);
-                    router.refresh();
-                }
-            )
-            .subscribe();
+        const initializeRealtime = async () => {
+            await supabase.realtime.setAuth();
 
-        console.log("Connected to messages channel", messagesChannel.topic);
-        console.log("Connected to users channel", usersChannel.topic);
+            usersChannelRef.current = supabase
+                .channel("users")
+                .on(
+                    "postgres_changes",
+                    {
+                        schema: "public",
+                        event: "*",
+                        table: "users",
+                        filter: `id=neq.${currentUser.id}`
+                    },
+                    (payload) => {
+                        console.log("User change → refresh", payload);
+                        router.refresh();
+                    }
+                )
+                .subscribe();
+
+            messagesChannelRef.current = supabase
+                .channel("messages")
+                .on(
+                    "postgres_changes",
+                    {
+                        schema: "public",
+                        event: "*",
+                        table: "messages"
+                    },
+                    (payload) => {
+                        console.log("Message change → refresh", payload);
+                        router.refresh();
+                    }
+                )
+                .subscribe();
+        };
+
+        initializeRealtime();
 
         return () => {
-            supabase.removeChannel(messagesChannel);
-            supabase.removeChannel(usersChannel);
+            if (usersChannelRef.current) {
+                supabase.removeChannel(usersChannelRef.current);
+
+                usersChannelRef.current = null;
+            }
+            if (messagesChannelRef.current) {
+                supabase.removeChannel(messagesChannelRef.current);
+
+                messagesChannelRef.current = null;
+            }
         };
-    }, [supabase, router]);
+    }, [currentUser]);
 };
 
 export default useRealtime;
