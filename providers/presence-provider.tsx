e09c7edf641f69c90a9d0f99c61c7da5e8f14c43
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2026-04-01 23:54:27 +0200

    package version

--- providers/presence-provider.tsx
+++ providers/presence-provider.tsx
@@ -44,7 +44,6 @@ export const PresenceProvider: FC<PresenceProviderProps> = ({ children }) => {
     });
 
     useEffect(() => {
-        supabase.realtime.setAuth(null);
         const presenceChannel = supabase.channel("presence#public");
 
         presenceChannel
